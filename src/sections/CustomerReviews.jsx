51e125578759aabfbd91bbc932275c5b01e25c52
Muhammad Adeel <muhammaddadeel90@gmail.com>
2023-09-18 00:29:42 +0500

    new changes

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { reviews } from "../constants";
 import ReviewCard from "../components/ReviewCard";
